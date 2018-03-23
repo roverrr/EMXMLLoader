003274a8c79510249a370faec05021c11ebc680d
Evgeny Smirnov <marsrover@macbook-pro-evgeny.local>
2018-03-23 21:32:02 +0300

    Initial Commit

---
+++ EMXMLLoader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EMXMLLoader
+//
+//  Created by Evgeny Smirnov on 23.03.2018.
+//  Copyright © 2018 Evgeny Smirnov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
